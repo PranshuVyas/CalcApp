a1089199269b3bdf1caa7b283b4a18177fe39584
PranshuVyas <38761690+PranshuVyas@users.noreply.github.com>
2019-01-12 11:58:29 -0500

    Numberpad Added

--- app/src/main/java/com/homeworkone/calcapp/CalcActivity.java
+++ app/src/main/java/com/homeworkone/calcapp/CalcActivity.java
@@ -22,19 +22,30 @@ public class CalcActivity extends AppCompatActivity {
     TextView txtOperation;
     @BindView(R.id.txt_result)
     TextView txtResult;
-    Button btnSum;
-    Button btnSubtract;
-    Button btnMultiply;
-    Button btnDivide;
-    Button btnMod;
-    Button btnLog;
-    Button btnClear;
+    Button btnOne;
+    Button btnTwo;
+    Button btnThree;
+    Button btnFour;
+    Button btnFive;
+    Button btnSix;
+    Button btnSeven;
+    Button btnEight;
+    Button btnNine;
+    Button btnTen;
+    Button btnZero;
+
+
 
     private SharedPreferences sharedPref;
 
     public static final String SHAREDPREFENCE_NAME = "CalcApp";
     public static final String KEY_VALUE_LEFT = "value_left";
     public static final String KEY_VALUE_RIGHT = "value_right";
+    public static final String ADD ="add";
+    public static final String SUBTRACT = "subtract";
+    public static final String MULTIPLY = "multiply";
+    public static final String DIVIDE = "divide";
+    public static final String MOD = "mod";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,80 +59,31 @@ public class CalcActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_sum)
     public void sum() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
-            double leftValue = Integer.parseInt(etxLeftValue.getText().toString());
-            double rightValue = Integer.parseInt(etxRightValue.getText().toString());
-            double result = leftValue + rightValue;
-            txtResult.setText("The result is : " + Double.toString(result));
-            txtOperation.setText("+");
-            savePreference();
-        }
+        performOperation(ADD);
     }
 
     @OnClick(R.id.btn_subtract)
     public void subtract() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
-            double leftValue = Integer.parseInt(etxLeftValue.getText().toString());
-            double rightValue = Integer.parseInt(etxRightValue.getText().toString());
-            double result = leftValue - rightValue;
-            txtResult.setText("The result is : " + Double.toString(result));
-            txtOperation.setText("-");
-            savePreference();
-            loadPreference();
-        }
+        performOperation(SUBTRACT);
     }
 
     @OnClick(R.id.btn_multiply)
     public void multiply() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
-            double leftValue = Integer.parseInt(etxLeftValue.getText().toString());
-            double rightValue = Integer.parseInt(etxRightValue.getText().toString());
-            double result = leftValue * rightValue;
-            txtResult.setText("The result is : " + Double.toString(result));
-            txtOperation.setText("X");
-            savePreference();
-        }
+        performOperation(MULTIPLY);
     }
 
     @OnClick(R.id.btn_divide)
     public void divide() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
-            double leftValue = Integer.parseInt(etxLeftValue.getText().toString());
-            double rightValue = Integer.parseInt(etxRightValue.getText().toString());
-            double result = (leftValue / rightValue);
-            txtResult.setText("The result is : " + Double.toString(result));
-            txtOperation.setText("/");
-            savePreference();
-        }
+      performOperation(DIVIDE);
     }
 
     @OnClick(R.id.btn_mod)
     public void mod() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
-            double leftValue = Integer.parseInt(etxLeftValue.getText().toString());
-            double rightValue = Integer.parseInt(etxRightValue.getText().toString());
-            double result = (leftValue % rightValue);
-            txtResult.setText("The result is : " + Double.toString(result));
-            txtOperation.setText("mod");
-            savePreference();
-        }
+        performOperation(MOD);
     }
 
     @OnClick(R.id.btn_clear)
     public void clear() {
-        if(isEmpty())
-            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
-        else {
             SharedPreferences.Editor prefEditor = sharedPref.edit();
             prefEditor.putString(KEY_VALUE_LEFT, "").apply();
             prefEditor.putString(KEY_VALUE_RIGHT, "").apply();
@@ -129,6 +91,98 @@ public class CalcActivity extends AppCompatActivity {
             etxRightValue.getText().clear();
             txtResult.setText("");
             txtOperation.setText("");
+    }
+
+    @OnClick(R.id.btn_one)
+    public void one(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("1");
+        }
+        else{
+            etxRightValue.setText("1");
+        }
+    }
+
+    @OnClick(R.id.btn_two)
+    public void two(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("2");
+        }
+        else{
+            etxRightValue.setText("2");
+        }
+    }
+    @OnClick(R.id.btn_three)
+    public void three(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("3");
+        }
+        else{
+            etxRightValue.setText("3");
+        }
+    }
+    @OnClick(R.id.btn_four)
+    public void four(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("4");
+        }
+        else{
+            etxRightValue.setText("4");
+        }
+    }
+    @OnClick(R.id.btn_five)
+    public void five(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("5");
+        }
+        else{
+            etxRightValue.setText("5");
+        }
+    }
+    @OnClick(R.id.btn_six)
+    public void six(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("6");
+        }
+        else{
+            etxRightValue.setText("6");
+        }
+    }
+    @OnClick(R.id.btn_seven)
+    public void seven(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("7");
+        }
+        else{
+            etxRightValue.setText("7");
+        }
+    }
+    @OnClick(R.id.btn_eight)
+    public void eight(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("8");
+        }
+        else{
+            etxRightValue.setText("8");
+        }
+    }
+    @OnClick(R.id.btn_nine)
+    public void nine(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("9");
+        }
+        else{
+            etxRightValue.setText("9");
+        }
+    }
+
+    @OnClick(R.id.btn_zero)
+    public void zero(){
+        if (etxLeftValue.getText().toString().isEmpty()){
+            etxLeftValue.setText("0");
+        }
+        else{
+            etxRightValue.setText("0");
         }
     }
 
@@ -141,6 +195,40 @@ public class CalcActivity extends AppCompatActivity {
         }
     }
 
+    private void performOperation(String operation){
+        double firstValue = 0;
+        double secondValue = 0;
+        double result = 0;
+        if(isEmpty())
+            Toast.makeText(this, "Invalid Entry", Toast.LENGTH_LONG).show();
+        else {
+            firstValue = Integer.parseInt(etxLeftValue.getText().toString());
+            secondValue = Integer.parseInt(etxRightValue.getText().toString());
+
+            if (operation == ADD) {
+                result = firstValue + secondValue;
+                txtOperation.setText("+");
+            } else if (operation == SUBTRACT) {
+                result = firstValue - secondValue;
+                txtOperation.setText("-");
+            } else if (operation == MULTIPLY) {
+                result = firstValue * secondValue;
+
+                txtOperation.setText("X");
+            } else if (operation == DIVIDE) {
+                result = firstValue / secondValue;
+
+                txtOperation.setText("/");
+            } else if (operation == MOD) {
+                result = firstValue % secondValue;
+                txtOperation.setText("mod");
+
+            }
+            savePreference();
+            txtResult.setText(Double.toString(result));
+        }
+
+    }
 
     private void savePreference() {
         SharedPreferences.Editor prefEditor = sharedPref.edit();
